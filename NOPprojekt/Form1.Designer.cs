72b3e4f48f3744e4f898a18cae652029220c5730
Ljubo-Grubisic <ljubo.grubisic1@gmail.com>
2023-05-16 15:43:47 +0200

    Game logic done mostly

--- NOPprojekt/Form1.Designer.cs
+++ NOPprojekt/Form1.Designer.cs
@@ -28,21 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxPlayer1 = new System.Windows.Forms.TextBox();
+            this.textBoxPlayer2 = new System.Windows.Forms.TextBox();
+            this.labelHint = new System.Windows.Forms.Label();
+            this.labelPlayer1 = new System.Windows.Forms.Label();
+            this.labelPlayer2 = new System.Windows.Forms.Label();
+            this.buttonGuess = new System.Windows.Forms.Button();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelCurrentTurn = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxPlayer1
+            // 
+            this.textBoxPlayer1.Location = new System.Drawing.Point(31, 106);
+            this.textBoxPlayer1.Name = "textBoxPlayer1";
+            this.textBoxPlayer1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPlayer1.TabIndex = 0;
+            // 
+            // textBoxPlayer2
+            // 
+            this.textBoxPlayer2.Location = new System.Drawing.Point(242, 106);
+            this.textBoxPlayer2.Name = "textBoxPlayer2";
+            this.textBoxPlayer2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPlayer2.TabIndex = 1;
+            // 
+            // labelHint
+            // 
+            this.labelHint.AutoSize = true;
+            this.labelHint.Location = new System.Drawing.Point(165, 109);
+            this.labelHint.Name = "labelHint";
+            this.labelHint.Size = new System.Drawing.Size(33, 13);
+            this.labelHint.TabIndex = 2;
+            this.labelHint.Text = "None";
+            // 
+            // labelPlayer1
+            // 
+            this.labelPlayer1.AutoSize = true;
+            this.labelPlayer1.Location = new System.Drawing.Point(28, 70);
+            this.labelPlayer1.Name = "labelPlayer1";
+            this.labelPlayer1.Size = new System.Drawing.Size(42, 13);
+            this.labelPlayer1.TabIndex = 3;
+            this.labelPlayer1.Text = "Player1";
+            // 
+            // labelPlayer2
+            // 
+            this.labelPlayer2.AutoSize = true;
+            this.labelPlayer2.Location = new System.Drawing.Point(239, 70);
+            this.labelPlayer2.Name = "labelPlayer2";
+            this.labelPlayer2.Size = new System.Drawing.Size(42, 13);
+            this.labelPlayer2.TabIndex = 4;
+            this.labelPlayer2.Text = "Player2";
+            // 
+            // buttonGuess
+            // 
+            this.buttonGuess.Location = new System.Drawing.Point(31, 224);
+            this.buttonGuess.Name = "buttonGuess";
+            this.buttonGuess.Size = new System.Drawing.Size(172, 57);
+            this.buttonGuess.TabIndex = 5;
+            this.buttonGuess.Text = "Guess";
+            this.buttonGuess.UseVisualStyleBackColor = true;
+            this.buttonGuess.Click += new System.EventHandler(this.buttonGuess_Click);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(32, 142);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(38, 13);
+            this.labelScore.TabIndex = 6;
+            this.labelScore.Text = "Score:";
+            // 
+            // labelCurrentTurn
+            // 
+            this.labelCurrentTurn.AutoSize = true;
+            this.labelCurrentTurn.Location = new System.Drawing.Point(28, 25);
+            this.labelCurrentTurn.Name = "labelCurrentTurn";
+            this.labelCurrentTurn.Size = new System.Drawing.Size(65, 13);
+            this.labelCurrentTurn.TabIndex = 7;
+            this.labelCurrentTurn.Text = "Current turn:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(620, 450);
+            this.Controls.Add(this.labelCurrentTurn);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.buttonGuess);
+            this.Controls.Add(this.labelPlayer2);
+            this.Controls.Add(this.labelPlayer1);
+            this.Controls.Add(this.labelHint);
+            this.Controls.Add(this.textBoxPlayer2);
+            this.Controls.Add(this.textBoxPlayer1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxPlayer1;
+        private System.Windows.Forms.TextBox textBoxPlayer2;
+        private System.Windows.Forms.Label labelHint;
+        private System.Windows.Forms.Label labelPlayer1;
+        private System.Windows.Forms.Label labelPlayer2;
+        private System.Windows.Forms.Button buttonGuess;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelCurrentTurn;
     }
 }
 
--- NOPprojekt/Form1.cs
+++ NOPprojekt/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,23 @@ namespace NOPprojekt
 {
     public partial class Form1 : Form
     {
+        private Player[] Players = { new Player { Name = "Player1", Score = 0 }, new Player { Name = "Player2", Score = 0 } };
+        /// <summary>
+        /// The random number the players are guessing
+        /// </summary>
+        private int RandomNumber;
+        /// <summary>
+        /// Represents which players turn it is
+        /// </summary>
+        private Player CurrentPlayer;
+        /// <summary>
+        /// Hint if the guess needs to be more or less
+        /// </summary>
+        private GuessHint GuessHint = GuessHint.None;
+        private string LabelScoreDefaultText;
+        private string LabelCurrentTurnDefaultText;
+        private Random Random;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +37,112 @@ namespace NOPprojekt
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.Random = new Random();
+            this.CurrentPlayer = Players[0];
+            this.LabelScoreDefaultText = labelScore.Text;
+            this.LabelCurrentTurnDefaultText = labelCurrentTurn.Text;
+            this.labelCurrentTurn.Text = LabelCurrentTurnDefaultText + " " + CurrentPlayer.Name;
+            this.RandomNumber = Random.Next(0, 101);
         }
+
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            int guess = -1;
+            if (CurrentPlayer.Name == Players[0].Name)
+            {
+                if (textBoxPlayer1.Text != "" && !textBoxPlayer1.Text.Contains("."))
+                {
+                    try
+                    {
+                        guess = int.Parse(textBoxPlayer1.Text);
+                    }
+                    catch { MessageBox.Show("Invalid Input"); return; }
+                    if (guess == RandomNumber)
+                    {
+                        Players[0].Score++;
+                        GuessHint = GuessHint.None;
+                        RandomNumber = Random.Next(0, 101);
+                    }
+                    else if (guess > RandomNumber)
+                    {
+                        GuessHint = GuessHint.Less;
+                    }
+                    else
+                    {
+                        GuessHint = GuessHint.More;
+                    }
+                    CurrentPlayer = Players[1];
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Input");
+                }
+                textBoxPlayer2.Text = "";
+            }
+            else if (CurrentPlayer.Name == Players[1].Name)
+            {
+                if (textBoxPlayer2.Text != "" && !textBoxPlayer2.Text.Contains("."))
+                {
+                    try
+                    {
+                        guess = int.Parse(textBoxPlayer2.Text);
+                    }
+                    catch { MessageBox.Show("Invalid Input"); return; }
+                    if (guess == RandomNumber)
+                    {
+                        Players[1].Score++;
+                        GuessHint = GuessHint.None;
+                        RandomNumber = Random.Next(0, 101);
+                    }
+                    else if (guess > RandomNumber)
+                    {
+                        GuessHint = GuessHint.Less;
+                    }
+                    else
+                    {
+                        GuessHint = GuessHint.More;
+                    }
+                    CurrentPlayer = Players[0];
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Input");
+                }
+                textBoxPlayer1.Text = "";
+            }
+            labelScore.Text = LabelScoreDefaultText + "\n" + Players[0].Name + ": " + Players[0].Score + "\n" + Players[1].Name + ": " + Players[1].Score;
+
+            if (CurrentPlayer.Name == Players[0].Name)
+            {
+                labelCurrentTurn.Text = LabelCurrentTurnDefaultText + " " + Players[0].Name;
+            }
+            else
+            {
+                labelCurrentTurn.Text = LabelCurrentTurnDefaultText + " " + Players[1].Name;
+            }
+            if (GuessHint == GuessHint.More)
+            {
+                labelHint.Text = "More";
+            }
+            else if (GuessHint == GuessHint.Less)
+            {
+                labelHint.Text = "Less";
+            }
+            else
+            {
+                labelHint.Text = "None";
+            }
+        }
+    }
+    public struct Player
+    {
+        public string Name;
+        public int Score;
+    }
+    public enum GuessHint
+    {
+        None,
+        More,
+        Less
     }
 }
